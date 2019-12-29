cfeb0f47d9ec9da5669f688fadba2caf3a495a07
4gus71n <agustin.tomas.larghi@gmail.com>
2019-12-29 17:09:05 -0300

    [feature/bookmark_room] Adding mapping functions to convert the DTOs into model classes, and vice-versa.

--- core/src/main/java/com/kimboo/core/mappers/MapperUtils.kt
+++ core/src/main/java/com/kimboo/core/mappers/MapperUtils.kt
@@ -2,6 +2,7 @@ package com.kimboo.core.mappers
 
 import com.kimboo.core.models.SquareRepository
 import com.kimboo.core.retrofit.responses.ApiSquareRespositoryResponse
+import com.kimboo.core.room.dto.DbSquareRepositoryDto
 
 /**
  * Classes with extended functions to turn backend responses into model classes
@@ -23,6 +24,37 @@ fun ApiSquareRespositoryResponse.toSquareRepository(): SquareRepository {
     return SquareRepository(
         name = name,
         stars = stargazersCount,
-        id = id.toString()
+        id = id.toString(),
+        isBookmarked = false // Default value
     )
+}
+
+fun List<SquareRepository>.toDto(): List<DbSquareRepositoryDto> {
+    return map {
+        it.toDto()
+    }
+}
+
+fun SquareRepository.toDto(): DbSquareRepositoryDto {
+    return DbSquareRepositoryDto(
+        id = id.toInt(),
+        isBookmarked = isBookmarked,
+        name = name,
+        stars = stars
+    )
+}
+
+fun DbSquareRepositoryDto.toSquareRepository(): SquareRepository {
+    return SquareRepository(
+        name = name,
+        stars = stars,
+        id = id.toString(),
+        isBookmarked = isBookmarked
+    )
+}
+
+fun List<DbSquareRepositoryDto>.fromDtoToSquareRepositoryList(): List<SquareRepository> {
+    return map {
+        it.toSquareRepository()
+    }
 }
